4549cf3f7591dbe2d5b9d47c7734e0130212ab37
Akhil Singh <akhilkumarsingh428@gmail.com>
2024-04-24 19:31:19 +0530

    gradient2

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,7 +12,7 @@ const Home = () => {
           className="text-4xl sm:text-7xl font-bold text-slate-500"
           style={{
             backgroundImage:
-              "linear-gradient(to right, #000000 0%, #ffffff  55%)",
+              "linear-gradient(to right, #000000 0%, #ffffff  70%)",
             WebkitBackgroundClip: "text",
             WebkitTextFillColor: "transparent",
           }}
